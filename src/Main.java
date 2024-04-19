d95079c984b96e1c0e996ca022cdbaaffe0f79cd
zilvis <zi.daugela@gmail.com>
2024-04-19 17:57:53 +0300

    TODO

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,15 @@ public class Main {
     public static void main(String[] args) {
         var menu = new MainUI();
         menu.menu();
+
+        // TODO Sukurti klasę AutomobiliųDuomenųModifikatorius
+        //  kurios funkcija gali priimti bet kokio tipo automobili - jeigu automobilis
+        //  yra varomas naftos kuru - jam ji turi leisti per parametrus pakeisti sanaudas, jeigu tai
+        //  elektromobilis - turi leisti pakeisti jo įkrovimo laiką.
+        //  Metodą pavadinti modifyParameter arba modifikuotiParametra
+
+
+        // AutomobiliųDuomenųModifikatoriui sukurti abstrakčia klasę, kurią paveldės NKA ir EV
+        // Sutvarkyti ivesties patikrinimus
     }
 }
